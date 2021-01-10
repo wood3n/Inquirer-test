c7e0f76ed51cf27a6a8acd84e1c01de6b0fb40b3
王凯 <wangkka1@163.com>
2021-01-10 12:09:09 +0800

    question-type

--- question.js
+++ question.js
@@ -3,15 +3,118 @@ const inquirer = require("inquirer");
 const questions = [
   {
     type: "editor",
-    name: "bio",
-    message: "Please write a short bio of at least 3 lines.",
-    validate: function (text) {
-      if (text.split("\n").length < 3) {
-        return "Must be at least 3 lines.";
-      }
-
-      return true;
-    },
+    name: "editor",
+    message: "test editor",
+  },
+  {
+    type: "input",
+    name: "input",
+    message: "test input",
+  },
+  {
+    type: "number",
+    name: "number",
+    message: "test number",
+  },
+  {
+    type: "confirm",
+    name: "confirm",
+    message: "test confirm",
+  },
+  {
+    type: "list",
+    name: "list",
+    message: "test list",
+    choices: [1, 2],
+  },
+  {
+    type: "rawlist",
+    name: "rawlist",
+    message: "test rawlist",
+    choices: [1, 2],
+  },
+  {
+    type: "expand",
+    name: "expand",
+    message: "test expand",
+    choices: [
+      {
+        key: "y",
+        name: "Overwrite",
+        value: "overwrite",
+      },
+      {
+        key: "a",
+        name: "Overwrite this one and all next",
+        value: "overwrite_all",
+      },
+      {
+        key: "d",
+        name: "Show diff",
+        value: "diff",
+      },
+      new inquirer.Separator(),
+      {
+        key: "x",
+        name: "Abort",
+        value: "abort",
+      },
+    ],
+  },
+  {
+    type: "checkbox",
+    name: "checkbox",
+    message: "test checkbox",
+    choices: [
+      new inquirer.Separator(" = The Meats = "),
+      {
+        name: "Pepperoni",
+      },
+      {
+        name: "Ham",
+      },
+      {
+        name: "Ground Meat",
+      },
+      {
+        name: "Bacon",
+      },
+      new inquirer.Separator(" = The Cheeses = "),
+      {
+        name: "Mozzarella",
+        checked: true,
+      },
+      {
+        name: "Cheddar",
+      },
+      {
+        name: "Parmesan",
+      },
+      new inquirer.Separator(" = The usual ="),
+      {
+        name: "Mushroom",
+      },
+      {
+        name: "Tomato",
+      },
+      new inquirer.Separator(" = The extras = "),
+      {
+        name: "Pineapple",
+      },
+      {
+        name: "Olives",
+        disabled: "out of stock",
+      },
+      {
+        name: "Extra cheese",
+      },
+    ],
+  },
+  {
+    type: "password",
+    name: "password",
+    mask: "*",
+    message: "test password",
   },
 ];
 
